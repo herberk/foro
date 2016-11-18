a46d74133c065c2e6feb02c245a54b330959e5f7
herberk <hermann@berkhoff.cl>
2016-11-18 11:11:41 -0400

    Use modifi MYSQL_USER MYSQL_USERNAM

--- config/database.php
+++ config/database.php
@@ -33,10 +33,9 @@ return [
     | Database Connections
     |--------------------------------------------------------------------------
     |
-    | Here are each of the database connections setup for your application.
-    | Of course, examples of configuring each database platform that is
-    | supported by Laravel is shown below to make development simple.
-    |
+    | Aquí están las configuraciones de las conexiones de la base de datos para su aplicación.
+    | Por supuesto, ejemplos de configuración de cada plataforma de base de datos que es
+    | Apoyado por Laravel se muestra a continuación para hacer el desarrollo simple.    |
     |
     | All database work in Laravel is done through the PHP PDO facilities
     | so make sure you have the driver for your particular database of
